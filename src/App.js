d769783e52acf31c6530f95b53b2abb6da4ac969
Abdelghafour <ghafourragg@gmail.com>
2022-02-07 16:00:40 +0100

    _

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import { Container } from "./components/styles/Container.styled";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Container>
+        <h1>HELLO HELL</h1>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
